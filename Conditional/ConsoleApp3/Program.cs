83e30a95f92d3c67daed35406835bd256c21e1e7
SabirGuliyev <sabir.guliyev@code.edu.az>
2023-09-27 18:57:02 +0400

    Control structure added

---
+++ Conditional/ConsoleApp3/Program.cs
@@ -0,0 +1,92 @@
+﻿
+
+//bool result = 20 > 10;
+
+
+//if (!result)
+//{
+//    Console.WriteLine("Duz hesablama");
+//}
+//else
+//{
+//    Console.WriteLine("Sehv hesablama");
+//}
+
+
+
+
+
+
+
+
+//byte age = 18;
+//string gender = "xanim";
+
+//if (age > 17 && gender=="xanim")
+//{
+//    Console.WriteLine("ishe qebul olduz");
+//}
+//else
+//{
+//    Console.WriteLine("Ugursuz namized");
+//}
+
+
+
+//if (choice==1)
+//{
+
+//}
+//else if (choice == 2)
+//{
+
+//}
+//else if(choice == 3)
+//{
+
+//}
+//byte choice =2;
+//switch (choice)
+//{
+//    case 1:
+//        Console.WriteLine("az dili secildi");
+//        break;
+//    case 2 or 3: 
+//        Console.WriteLine("diger diller");
+//        break;
+
+//    //default:
+//    //    Console.WriteLine("bele bir secimimiz yoxdur");
+//    //    break;
+
+//}
+
+//string day = "sunday";
+//switch (day)
+//{
+//    case "monday":
+
+//        Console.WriteLine("Ish gunu");
+//        break;
+//    case "sunday":
+//        Console.WriteLine("Istirahet");
+//        break;
+
+//}
+
+
+int num = 0;
+
+if (num>0)
+{
+    Console.WriteLine("Musbet");
+}
+else if (num < 0)
+{
+    Console.WriteLine("menfi");
+}
+else
+{
+    Console.WriteLine("neytral");
+}
+
---
+++ Loops/Loops/Program.cs
@@ -0,0 +1,113 @@
+﻿int num = 10;
+
+//string answer = "";
+//do
+//{
+//    Console.WriteLine("dovr devam etsin?");
+//    answer = Console.ReadLine();
+
+//} while (answer == "yes");
+
+//Console.WriteLine("dovr devam etsin?");
+//answer = Console.ReadLine();
+//while (answer == "yes")
+//{
+//    Console.WriteLine("dovr devam etsin?");
+//  answer = Console.ReadLine();
+//}
+
+
+//string choice = "no";
+//while (choice=="no")
+//{
+//    Console.WriteLine("Dovr bitsin?");
+//    choice=Console.ReadLine();
+//}
+//int i = 0;
+//while (i<10)
+//{
+
+//}
+
+//int a = 0;
+//while (a<10)
+//{
+
+//}
+
+
+//for (int i = 1; i < 50; i++)
+//{
+//    if (i == 20)
+//    {
+//        Console.WriteLine($"buna beraberdir {i}");
+//        break;
+//    }
+
+//    Console.WriteLine(i);
+//}
+
+//for (; ;)
+//{
+//    Console.WriteLine("Hello world");
+//}
+
+
+//for (int i = 20; i >0; i-=2)
+//{
+//    Console.WriteLine(i);
+//}
+
+
+//Task 1
+
+//for (int i = 1; i < 90; i++)
+//{
+//    if (i%2==0)
+//    {
+//        Console.WriteLine(i);
+//    }
+//}
+
+//Task 2
+
+
+//int sum = 0;
+//for (int i = 1; i < 100; i++)
+//{
+
+//    sum += i;
+
+//}
+
+//Console.WriteLine(sum);
+
+
+//Task 3
+
+
+int a = 20;
+int b = 20;
+
+int less = 0;
+int greater = 0;
+if (a < b)
+{
+    less = a;
+    greater = b;
+}
+else
+{
+    less = b;
+    greater = a;
+}
+
+
+for (int i = less; i < greater; i++)
+{
+    if (i % 3 == 0)
+    {
+        Console.WriteLine(i);
+    }
+}
+
