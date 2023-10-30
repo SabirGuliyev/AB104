d7679d58481caa55e7c546191fd2fe3960998317
SabirGuliyev <sabir.guliyev@code.edu.az>
2023-10-30 11:04:26 +0400

    Joins added

---
+++ SQLJoins.sql
@@ -0,0 +1,45 @@
+﻿CREATE DATABASE AB104Relation
+
+USE AB104Relation
+
+CREATE TABLE Universities(
+Id INT PRIMARY KEY IDENTITY,
+Name VARCHAR(100) NOT NULL UNIQUE,
+)
+
+CREATE TABLE Subjects(
+Id INT PRIMARY KEY IDENTITY,
+Name VARCHAR(100) NOT NULL UNIQUE,
+)
+
+CREATE TABLE Students(
+Id INT PRIMARY KEY IDENTITY,
+Name NVARCHAR(25) NOT NULL,
+Surname NVARCHAR(25) DEFAULT 'XXX',
+UniversityId INT REFERENCES Universities(Id)
+)
+
+--CREATE TABLE StudentSubjects(
+
+--)
+
+
+INSERT INTO Students VALUES(N'Zülfiyyə',N'Qurbanova',2)
+
+SELECT s.Name, u.Name AS [University Name] FROM Students AS s
+FULL OUTER JOIN Universities AS u
+ON s.UniversityId=u.Id
+WHERE s.UniversityId IS NULL
+
+
+
+CREATE TABLE Employees(
+Id INT PRIMARY KEY IDENTITY,
+Name NVARCHAR(25) NOT NULL,
+DependId INT REFERENCES Employees(Id)
+)
+
+
+SELECT Employees.Name, Depend.Name as [Depend on] FROM Employees
+LEFT JOIN Employees AS Depend
+ON Employees.DependId=Depend.Id
